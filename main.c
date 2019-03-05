f0bb978d905df07bfdbdac0576f8735004a11bcb
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-03-05 10:13:20 +0700

    Add func kbhit

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int int main()
-{
-	/* code */
-	return 0;
+
+int kbhit() {
+  struct timeval tv;
+  fd_set fds;
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  FD_ZERO(&fds);
+  FD_SET(STDIN_FILENO, &fds);
+  select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+  return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+int int main() {
+  /* code */
+  return 0;
 }
