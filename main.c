e607f4db322041f538eaa3dd31391505a0311627
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-03-05 10:14:43 +0700

    Add func nonblock

--- main.c
+++ main.c
@@ -11,6 +11,25 @@ int kbhit() {
   return FD_ISSET(STDIN_FILENO, &fds);
 }
 
+void nonblock(int state) {
+  struct termios ttystate;
+
+  // Get the terminal state.
+  tcgetattr(STDIN_FILENO, &ttystate);
+
+  if (state == NB_ENABLE) {
+    // Turn off canonical mode.
+    ttystate.c_lflag &= ~ICANON;
+    // Minimum of number input read.
+    ttystate.c_cc[VMIN] = 1;
+  } else if (state == NB_DISABLE) {
+    // Turn on canonical mode.
+    ttystate.c_lflag |= ICANON;
+  }
+  // Set the terminal attributes.
+  tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+}
+
 int int main() {
   /* code */
   return 0;
